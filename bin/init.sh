7f5db70e1f6da3cc1bec02777bb860d92bf4a66f
hoanganh <hoahoanganh20012001@gmail.com>
2023-12-16 00:03:58 +0700

    update bash script

--- bin/init.sh
+++ bin/init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash 
 
-gnome-terminal --tab --title="Loader" --command="bash -c 'bash ./loader.sh; exec bash'"
-# gnome-terminal --tab --title="Process" --command="bash -c 'bash process.sh; exec bash'"
-# gnome-terminal --tab --title="Scraper" --command="bash -c 'bash scraper.sh; exec bash'"
-# gnome-terminal --tab --title="Server" --command="bash -c 'bash server.sh; exec bash'"
-# gnome-terminal --tab --title="Client" --command="bash -c 'bash client.sh; exec bash'"
+set -e
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+DIR=$(dirname "$SCRIPT_DIR")
+
+gnome-terminal --tab --title="Loader" --command="bash -c 'bash $DIR/bin/loader.sh; exec bash'"
+# gnome-terminal --tab --title="Process" --command="bash -c 'bash  $DIR/bin/process.sh; exec bash'"
+# gnome-terminal --tab --title="Scraper" --command="bash -c 'bash  $DIR/bin/scraper.sh; exec bash'"
+# gnome-terminal --tab --title="Server" --command="bash -c 'bash  $DIR/bin/server.sh; exec bash'"
+# gnome-terminal --tab --title="Client" --command="bash -c 'bash  $DIR/bin/client.sh; exec bash'"e
